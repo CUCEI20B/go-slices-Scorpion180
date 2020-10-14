c52c7e43e37c6a3fae5916a37fe373f67ee709d9
efren <efren.hernandez@alumnos.udg.mx>
2020-10-13 20:15:53 -0500

    V1

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var size int = 0
+	var i int = 0
+	var sum int = 0
+	var acumm int = 0
+	var list []int
+
+	fmt.Scanln(&size)
+
+	for i < size {
+		fmt.Scanln(&sum)
+		list = append(list, sum)
+		acumm = acumm + sum
+		i = i + 1
+	}
+
+	fmt.Print(acumm)
+
 }
